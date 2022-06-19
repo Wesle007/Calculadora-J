0639e424596e797a1a019d0d80306c5198c9daa4
weslemarques <wesleremarques@gmail.com>
2022-06-18 21:37:30 -0300

    Melhorando Calculadora

--- src/Menu.java
+++ src/Menu.java
@@ -2,31 +2,32 @@ import javax.swing.JOptionPane;
 
 public class Menu {
     private int opcao;
+    private String operacao;
 
    public int retornarOpcao(){
    
       Boolean c =true;
     while(c){
-        int op = Integer.parseInt(JOptionPane.showInputDialog( "|---CALCULADORA---|\n|--(1) ADIÇÃO-----|\n|--(2) SUBTRAÇÃO--|\n|--(3) MULTIPLICAR|\n|--(4) DIVIDIR----|"));
+        int op = Integer.parseInt(JOptionPane.showInputDialog( "|---CALCULADORA---|\n|--(1) ADIÇÃO-----------|\n|--(2) SUBTRAÇÃO----|\n|--(3) MULTIPLICAR---|\n|--(4) DIVIDIR------------|"));
         setOpcao(op);
 
-        if(getOpcao()== 1 ||getOpcao()== 2 || getOpcao()== 3 || getOpcao()== 4){
-            JOptionPane.showMessageDialog(null, "OPÇÃO: " + opcao);
+        if(opcao== 1 ||opcao== 2 || opcao== 3 || opcao== 4){
+            switch(opcao){
+                case 1: operacao = "ADIÇÂO";
+                case 2: operacao = "SUBTRAÇÃO";
+                case 3: operacao = "MULTIPLICAÇÃO";
+                case 4: operacao = "DIVISÃO";
+            }
+
+            JOptionPane.showMessageDialog(null, "OPÇÃO: " + operacao);
             c = false;
             
             
-        }
-            else{
+        }else{
                 JOptionPane.showMessageDialog(null, "DIGITE UMA DAS OPCÕES");
-                
-            
-       }
+                 }
     }
     return opcao;
-   
-   
-
-    
 }
 
 /**
